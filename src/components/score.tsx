8fa6d10e906d2364c852529bd5bdb84bc75cc89a
Ahmed Mabrouk <ahmed.ely.mabrouk@gmail.com>
2024-10-22 15:31:42 +0100

    fix: fix some design details

--- src/components/score.tsx
+++ src/components/score.tsx
@@ -4,9 +4,15 @@ interface IScore {
 }
 export const Score = ({ x, o }: IScore) => {
   return (
-    <div className="flex items-center font-semibold text-2xl">
-      X<div className="w-8 h-8 bg-light-gray text-icon-blue mx-2">{x}</div>O
-      <div className="w-8 h-8 bg-light-gray text-icon-blue ml-2">{o}</div>
+    <div className="flex items-center font-semibold text-xl">
+      X
+      <div className="w-7 h-8 bg-light-gray text-icon-blue mr-3 ml-1 rounded-md">
+        {x}
+      </div>
+      O
+      <div className="w-7 h-8 bg-light-gray text-icon-blue ml-1 rounded-md">
+        {o}
+      </div>
     </div>
   );
 };
